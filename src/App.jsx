efd2c8228924cb92847829c35f2292ba9639ec73
Noma <wiostz98kr@gmail.com>
2021-10-04 23:16:53 +0900

    Add devLog page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import styles from './App.module.scss';
 import About from './pages/about/About';
+import DevLog from './pages/devLog/DevLog';
 import Header from './components/header/Header';
 import Home from './pages/home/Home';
 import Skills from './pages/skills/Skills';
@@ -13,6 +14,7 @@ function App() {
       <Home tabRefs={tabRefs} />
       <About tabRefs={tabRefs} />
       <Skills tabRefs={tabRefs} />
+      <DevLog tabRefs={tabRefs} />
     </>
   );
 }
---
+++ src/pages/devLog/DevLog.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styles from './DevLog.module.scss';
+import github from '../../images/logs/github.PNG';
+import velog from '../../images/logs/velog.PNG';
+
+function DevLog({ tabRefs }) {
+    return (
+        <article ref={(el) => (tabRefs.current[3] = el)} className={styles.DevLog}>
+            <h1>DevLog</h1>
+            <div className={styles.container}>
+                <section>
+                    <h3>Github</h3>
+                    <img src={github} alt="github" />
+                    <a href="https://velog.io/@wiostz98kr" target="_blank" rel="noopener noreferrer">바로가기
+                    </a>
+                </section>
+                <section>
+                    <h3>Dev Blog</h3>
+                    <img src={velog} alt="velog" />
+                    <a href="https://github.com/Noma98" target="_blank" rel="noopener noreferrer">
+                        바로가기
+                    </a>
+                </section>
+            </div>
+        </article>
+    )
+}
+
+export default DevLog
